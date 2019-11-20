a63ea73d91dda9e0ad1a2705b4de838418256c5d
Markus Røyset <maroider@protonmail.com>
2019-11-20 08:11:17 +0100

    Fix issue with animated cursors not working
    This is achieved by using a newer API to load and copy around cursors..

--- src/cursor.rs
+++ src/cursor.rs
@@ -1,14 +1,19 @@
 use std::{io, ops::Drop, path::Path, ptr};
 
 use winapi::{
-    shared::{windef::HCURSOR, winerror::ERROR_FILE_NOT_FOUND},
+    shared::{ntdef::HANDLE, windef::HICON, winerror::ERROR_FILE_NOT_FOUND},
     um::{
         errhandlingapi::GetLastError,
         winuser::{
-            CopyIcon, GetCursor, LoadCursorFromFileW, LoadCursorW, SetSystemCursor,
+            CopyImage, LoadImageW, SetSystemCursor, IMAGE_CURSOR, LR_LOADFROMFILE, LR_SHARED,
             MAKEINTRESOURCEW,
         },
     },
+};
+
+use missing_from_winapi::{
+    OCR_APPSTARTING, OCR_CROSS, OCR_HAND, OCR_IBEAM, OCR_NO, OCR_NORMAL, OCR_SIZEALL, OCR_SIZENESW,
+    OCR_SIZENS, OCR_SIZENWSE, OCR_SIZEWE, OCR_UP, OCR_WAIT,
 };
 
 #[derive(Debug)]
@@ -19,7 +24,7 @@ pub struct ReplacedCursor {
 
 impl ReplacedCursor {
     pub fn revert(&self) {
-        unsafe { SetSystemCursor(self.cursor.handle, self.kind.as_id()) };
+        unsafe { SetSystemCursor(self.cursor.handle as HICON, self.kind.as_id()) };
     }
 }
 
@@ -31,7 +36,7 @@ impl Drop for ReplacedCursor {
 
 #[derive(Debug, PartialEq)]
 pub struct Cursor {
-    handle: HCURSOR,
+    handle: HANDLE,
 }
 
 impl Cursor {
@@ -40,7 +45,16 @@ impl Cursor {
         let path = path.to_string_lossy();
 
         let utf16_path: Vec<u16> = path.encode_utf16().chain(0..=0).collect();
-        let handle = unsafe { LoadCursorFromFileW(utf16_path.as_ptr()) };
+        let handle = unsafe {
+            LoadImageW(
+                ptr::null_mut(),
+                utf16_path.as_ptr(),
+                IMAGE_CURSOR,
+                0,
+                0,
+                LR_SHARED | LR_LOADFROMFILE,
+            )
+        };
 
         if handle.is_null() {
             let error_code = unsafe { GetLastError() };
@@ -56,24 +70,22 @@ impl Cursor {
         }
     }
 
-    #[allow(dead_code)]
-    pub fn current_system() -> Option<Self> {
-        let handle = unsafe { GetCursor() };
-        if handle.is_null() {
-            None
-        } else {
-            Some(Self { handle })
-        }
-    }
-
     pub fn load_system(kind: CursorKind) -> Self {
-        let cursor = unsafe { LoadCursorW(ptr::null_mut(), MAKEINTRESOURCEW(kind.as_id() as u16)) };
+        let cursor = unsafe {
+            LoadImageW(
+                ptr::null_mut(),
+                MAKEINTRESOURCEW(kind.as_id() as u16),
+                IMAGE_CURSOR,
+                0,
+                0,
+                LR_SHARED,
+            )
+        };
         if cursor.is_null() {
             panic!("TODO: Handle errors")
         }
 
-        let handle = unsafe { CopyIcon(cursor) };
-
+        let handle = unsafe { CopyImage(cursor, IMAGE_CURSOR, 0, 0, 0) };
         if handle.is_null() {
             panic!("TODO: Handle errors")
         }
@@ -84,7 +96,7 @@ impl Cursor {
     pub fn replace_system(self, kind: CursorKind) -> ReplacedCursor {
         let cursor = Self::load_system(kind);
         // TODO: Handle errors
-        unsafe { SetSystemCursor(self.handle, kind.as_id()) };
+        unsafe { SetSystemCursor(self.handle as HICON, kind.as_id()) };
         ReplacedCursor { cursor, kind }
     }
 }
@@ -100,8 +112,6 @@ pub enum CursorKind {
     Crosshair,
     /// Hand
     Hand,
-    /// Arrow and question mark
-    Help,
     /// I-Beam
     Ibeam,
     /// Slashed circle
@@ -109,7 +119,7 @@ pub enum CursorKind {
     /// Four-pointed arrow pointing north, south, east, and west
     SizeAll,
     /// Double-pointed arrow pointing northeast and southwest
-    SizeSw,
+    SizeNeSw,
     /// Double-pointed arrow pointing north and south
     SizeNs,
     /// Double-pointed arrow pointing northwest and southeast
@@ -125,20 +135,19 @@ pub enum CursorKind {
 impl CursorKind {
     pub fn as_id(self) -> u32 {
         match self {
-            Self::AppStarting => 32650,
-            Self::Normal => 32512,
-            Self::Crosshair => 32515,
-            Self::Hand => 32649,
-            Self::Help => 32651,
-            Self::Ibeam => 32513,
-            Self::No => 32648,
-            Self::SizeAll => 32646,
-            Self::SizeSw => 32643,
-            Self::SizeNs => 32645,
-            Self::SizeNwSe => 32642,
-            Self::SizeWe => 32644,
-            Self::Up => 32516,
-            Self::Wait => 32514,
+            Self::AppStarting => OCR_APPSTARTING,
+            Self::Normal => OCR_NORMAL,
+            Self::Crosshair => OCR_CROSS,
+            Self::Hand => OCR_HAND,
+            Self::Ibeam => OCR_IBEAM,
+            Self::No => OCR_NO,
+            Self::SizeAll => OCR_SIZEALL,
+            Self::SizeNeSw => OCR_SIZENESW,
+            Self::SizeNs => OCR_SIZENS,
+            Self::SizeNwSe => OCR_SIZENWSE,
+            Self::SizeWe => OCR_SIZEWE,
+            Self::Up => OCR_UP,
+            Self::Wait => OCR_WAIT,
         }
     }
 
@@ -149,11 +158,10 @@ impl CursorKind {
             Self::Normal => "Normal",
             Self::Crosshair => "Crosshair",
             Self::Hand => "Hand",
-            Self::Help => "Help",
             Self::Ibeam => "IBeam",
             Self::No => "No",
             Self::SizeAll => "SizeAll",
-            Self::SizeSw => "SizeSW",
+            Self::SizeNeSw => "SizeNeSw",
             Self::SizeNs => "SizeNS",
             Self::SizeNwSe => "SizeNWSE",
             Self::SizeWe => "SizeWE",
@@ -161,4 +169,27 @@ impl CursorKind {
             Self::Wait => "Wait",
         }
     }
+}
+
+#[allow(dead_code)]
+mod missing_from_winapi {
+    pub const OCR_NORMAL: u32 = 32512;
+    pub const OCR_IBEAM: u32 = 32513;
+    pub const OCR_WAIT: u32 = 32514;
+    pub const OCR_CROSS: u32 = 32515;
+    pub const OCR_UP: u32 = 32516;
+    /// Use OCR_SIZEALL instead
+    pub const OCR_SIZE: u32 = 32640;
+    /// Use OCR_NORMAL instead
+    pub const OCR_ICON: u32 = 32641;
+    pub const OCR_SIZENWSE: u32 = 32642;
+    pub const OCR_SIZENESW: u32 = 32643;
+    pub const OCR_SIZEWE: u32 = 32644;
+    pub const OCR_SIZENS: u32 = 32645;
+    pub const OCR_SIZEALL: u32 = 32646;
+    /// Use OIC_WINLOGO instead
+    pub const OCR_ICOCUR: u32 = 32647;
+    pub const OCR_NO: u32 = 32648;
+    pub const OCR_HAND: u32 = 32649;
+    pub const OCR_APPSTARTING: u32 = 32650;
 }
